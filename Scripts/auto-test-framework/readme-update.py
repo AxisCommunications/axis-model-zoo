119efee93fdcc94c1d2f6e55bd68d9320fdefb32
Angelo DelliSanti <angelod@pcczc21093t1.se.axis.com>
2022-11-04 13:34:09 +0100

    wip

--- Scripts/auto-test-framework/readme-update.py
+++ Scripts/auto-test-framework/readme-update.py
@@ -1,12 +1,12 @@
 import os
 import re  
 
-tokens = ['A8_tf1_mnv2']
+tokens = ['A8_tf1_mnv2','A7_tf1_mnv1','A7_tf2_mnv3','cv25_tf1_mnv2']
 token_parameters = { 
     "/artpec7/tf2_mobilenet_v1_1.0_224_ptq_edgetpu.tflite" : "A7_tf2_mnv1",
-    "/artpec7/tf2_mobilenet_v3_edgetpu_1.0_224_ptq_edgetpu.tflite" : "A7_tf2_mnv2",
+    "/artpec7/tf2_mobilenet_v3_edgetpu_1.0_224_ptq_edgetpu.tflite" : "A7_tf2_mnv3",
     "/artpec8/mobilenet_v2_1.0_224_quant.tflite" :"A8_tf1_mnv2",
-    "/cv25/mobilenetv2_cavalry.bin": "CV25_tf1_mnv2"
+    "/cv25/mobilenetv2_cavalry.bin": "cv25_tf1_mnv2"
     }
 
 #read md file
@@ -53,7 +53,7 @@ def main():
     inference_times = extract_inference_time(larod_output)
     for token in tokens:
         token_index = find_token(md_file, token)
-        if token_index != -1:
+        if token_index != -1 and token in inference_times:
             value_to_add = inference_times[token]
             table = generate_table(value_to_add, token)
             rewrite_md_file(file_name, md_file, token, table)
