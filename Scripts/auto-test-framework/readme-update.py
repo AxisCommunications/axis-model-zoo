84948b8f92a6724e6e3f68cf4b235fc679b2bcf3
Angelo DelliSanti <angelod@pcczc21093t1.se.axis.com>
2022-11-04 14:05:46 +0100

    wip

--- Scripts/auto-test-framework/readme-update.py
+++ Scripts/auto-test-framework/readme-update.py
@@ -1,8 +1,9 @@
 import os
 import re  
 
-tokens = ['A8_tf1_mnv2','A7_tf1_mnv1','A7_tf2_mnv3','cv25_tf1_mnv2']
+tokens = ['A8_tf1_mnv2','A7_tf2_mnv1','A7_tf2_mnv3','cv25_tf1_mnv2']
 token_parameters = { 
+    
     "/artpec7/tf2_mobilenet_v1_1.0_224_ptq_edgetpu.tflite" : "A7_tf2_mnv1",
     "/artpec7/tf2_mobilenet_v3_edgetpu_1.0_224_ptq_edgetpu.tflite" : "A7_tf2_mnv3",
     "/artpec8/mobilenet_v2_1.0_224_quant.tflite" :"A8_tf1_mnv2",
