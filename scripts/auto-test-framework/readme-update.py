f9606b979eb2275bd9f46026de1bd661e98b2ef1
Angelo Delli Santi <dellisanti.angelo@gmail.com>
2023-01-09 10:44:09 +0100

    EfficientNet cv25 (#18)
    * Update README.md
    * Add model 
    * Update readme-update.py
    * adding link to model in acap
    
    Change-Id: I7fe50536407514b46f28a74e01329bb596293332
    
    * Updating test results
    
    * Updating test results
    
    * Updating test results
    
    Co-authored-by: Angelo DelliSanti <angelod@axis.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- scripts/auto-test-framework/readme-update.py
+++ scripts/auto-test-framework/readme-update.py
@@ -1,14 +1,15 @@
 import os
 import re  
 
-tokens = ['A8_tf1_mnv2','A7_tf1_mnv2','A7_tf2_mnv2','A7_tf2_mnv3','cv25_tf1_mnv2']
+tokens = ['A8_tf1_mnv2','A7_tf1_mnv2','A7_tf2_mnv2','A7_tf2_mnv3','cv25_tf1_mnv2','cv25_tf1_ens']
 token_parameters = { 
 
     "/artpec7/mobilenet_v2_1.0_224_quant_edgetpu.tflite" : "A7_tf1_mnv2",
     "/artpec7/tf2_mobilenet_v2_1.0_224_ptq_edgetpu.tflite" : "A7_tf2_mnv2",
     "/artpec7/tf2_mobilenet_v3_edgetpu_1.0_224_ptq_edgetpu.tflite" : "A7_tf2_mnv3",
-    "/artpec8/mobilenet_v2_1.0_224_quant.tflite" :"A8_tf1_mnv2",
-    "/cv25/mobilenetv2_cavalry.bin": "cv25_tf1_mnv2"
+    "/artpec8/mobilenet_v2_1.0_224_quant.tflite" : "A8_tf1_mnv2",
+    "/cv25/mobilenetv2_cavalry.bin" : "cv25_tf1_mnv2",
+    "/cv25/EfficientNet-S.bin" : "cv25_tf1_ens"
     }
 
 #read md file
